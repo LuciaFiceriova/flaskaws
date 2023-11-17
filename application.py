a8cf3b905bdc67ab4ed412f4711c464812e4491a
LuciaFiceriova <lucia.ficeriova@gmail.com>
2023-11-17 17:34:41 +0000

    first commit

--- application.py
+++ application.py
@@ -1,14 +1,14 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
+application = Flask(__name__)
 
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:''@localhost/flaskaws'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://admin:P!ATD2001@flaskaws.c9789jpe9v1g.eu-west-1.rds.amazonaws.com/flaskaws'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.secret_key = "somethingunique"
+# application.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:''@localhost/flaskaws'
+application.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://admin:P!ATD2001@flaskaws.c9789jpe9v1g.eu-west-1.rds.amazonaws.com/flaskaws'
+application.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+application.secret_key = "somethingunique"
 
-db = SQLAlchemy(app)
+db = SQLAlchemy(application)
 
 
 class Member(db.Model):
@@ -22,14 +22,14 @@ class Member(db.Model):
         self.role = role
         
 
-@app.route('/')
+@application.route('/')
 
 def index():
     members = Member.query.all()
     return render_template('index.html', members=members)
 
 
-@app.route('/add/', methods = ['POST'])
+@application.route('/add/', methods = ['POST'])
 
 def insert_member():
     if request.method =="POST":
@@ -43,7 +43,7 @@ def insert_member():
         return redirect(url_for('index'))
 
 
-@app.route('/delete/<id>/', methods = ['GET', 'POST'])
+@application.route('/delete/<id>/', methods = ['GET', 'POST'])
 
 def delete(id):
     my_data = Member.query.get(id)
@@ -54,4 +54,4 @@ def delete(id):
 
 
 if __name__ == "__main__":
-    app.run(debug = True)
+    application.run(debug = True)
